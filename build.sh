a2476a1587268546f18ee50e7724d41d764f128b
tchab <dev@chabs.name>
2021-12-31 10:59:35 +0000

    Fix Github Actions to use secrets.

--- build.sh
+++ build.sh
@@ -3,16 +3,20 @@
 # debug
 #set -x
 
+if [[ -z "${SITE_PASSWORD}" ]]; then
+    # Read Password from stdin
+    echo -n "Password: " 
+    read -r -s site_password
+    echo
+else
+    site_password="${SITE_PASSWORD}"
+fi
+
 # failfast
 set -eEuo pipefail
 
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 build_dir="${script_dir}/dist"
-
-# Read Password
-echo -n "Password: " 
-read -r -s site_password
-echo
 
 mkdir -p "${build_dir}"
 cp -r img "${build_dir}"
